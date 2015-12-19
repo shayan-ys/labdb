5c7781f217db5b7bf4fab7404b6af1ecc4c8fa54
shayan-ys <shayan.yousefian@bamilo.com>
2015-12-19 09:21:51 +0330

    patient pages and some useful function such as table generator and checkSign

---
+++ header.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Shayanyousefian
+ * Date: 12/19/15
+ * Time: 1:40 AM
+ */
---
+++ table_generator.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Shayanyousefian
+ * Date: 12/19/15
+ * Time: 8:53 AM
+ */
